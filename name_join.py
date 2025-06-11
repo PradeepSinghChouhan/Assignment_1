0f3d7b04eca332901f7b7dd9ff856c5ea7d979a6
PradeepSinghChouhan <pradeep.me.aiet@gmail.com>
2025-06-12 01:03:53 +0530

    Add files via upload

---
+++ name_join.py
@@ -0,0 +1,4 @@
+A=input("what is your first name: ")
+B=input("what is your last name: ")
+c=A+ B
+print("Hello mr.",c+ "!  welocome to python programming")
---
+++ test_import.py
@@ -0,0 +1,33 @@
+A=input("enter the first number: ")
+B=input("enter the second number: ")
+A=float(A)
+B=float(B)
+
+C=A+B
+D=A-B
+E=A*B
+F=A/B
+print()
+print()
+print("Addition\t\t:", C)
+print("Subtraction\t\t:", D)
+print("Multiplication\t:", E)
+print("Division\t\t:", F)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
